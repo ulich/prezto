1db5eed2cff8ac7a25627b726702bae8f8317438
Timo Ulich <timo.ulich@asideas.de>
2015-06-03 22:45:04 +0200

    adapt modules, LS_COLORS, prompt, editor and enable auto-tile

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -79,10 +79,10 @@ else
   # BSD Core Utilities
   if zstyle -t ':prezto:module:utility:ls' color; then
     # Define colors for BSD ls.
-    export LSCOLORS='exfxcxdxbxGxDxabagacad'
+    export LSCOLORS='Exgxcxdxcxegedabagacad'
 
     # Define colors for the completion system.
-    export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'
+    export LS_COLORS='di=1;34;40:ln=36;40:so=32;40:pi=33;40:ex=32;40:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:'
 
     alias ls="${aliases[ls]:-ls} -G"
   else
